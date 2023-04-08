332b974650dcc141c71d0455120370d83785586b
ZUBAIR-RAHMAN <zubair.rahman89@gmail.com>
2023-04-08 22:03:35 +0600

    total  page style bgColor addedd navbar style and  varients added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,22 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
+
 const Navbar = () => (
-  <nav>
+  <motion.nav
+    varients={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className="absulate w[50%] inset-0 gradient-01" />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img />
+    </div>
     navbar
-  </nav>
+  </motion.nav>
 );
 
 export default Navbar;
